2c5afb6fd8526e8b2791605a7fcb1470b4c6ce60
Enriqueescr <enrique.escribano@bootcamp-upgrade.com>
2025-03-05 14:39:15 +0100

    desarrollador Remoto
    Realizamos la clase desarrollador remoto

---
+++ sistema-gestion-empleados/src/DesarrolladorRemoto.java
@@ -0,0 +1,8 @@
+public class DesarrolladorRemoto extends Desarrollador implements TrabajadorRemoto{
+
+
+    public DesarrolladorRemoto(String nombre, int edad, double salarioBase){
+        super(nombre, edad, salarioBase);
+    }
+
+}
