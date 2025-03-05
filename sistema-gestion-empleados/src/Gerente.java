f745fdb5d38119448ce3ea70745d670a39d43dfd
Enriqueescr <enrique.escribano@bootcamp-upgrade.com>
2025-03-05 13:42:14 +0100

    Update Gerente.java

--- sistema-gestion-empleados/src/Gerente.java
+++ sistema-gestion-empleados/src/Gerente.java
@@ -1,3 +1,21 @@
-public class Gerente {
+public class Gerente extends Empleado {
+    int bono;
+
+    public Gerente(String nombre, int edad, double salarioBase){
+        this.nombre = nombre;
+        this.edad= nombre;
+        this.salarioBase = salarioBase;
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
