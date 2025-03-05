8db355d1704b700e117db676fad1ddcf0a185255
daridoto <rudasa7@gmail.com>
2025-03-05 13:43:10 +0100

    Proceso creacion de Empleado

--- sistema-gestion-empleados/src/Empleado.java
+++ sistema-gestion-empleados/src/Empleado.java
@@ -1,3 +1,30 @@
-public class Empleado {
+public abstract class Empleado {
+
+  //Escribo los atributos de mi clase abstracta
+  private String nombre;
+  private int edad;
+  private double salarioBase;
+
+  //A continuación escribo el constructor de la clase Empleado
+  public Empleado(String nombre, int edad, int salarioBase){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.salarioBase = salarioBase;
+    if(edad <= 18){
+      System.err.println("No tienes edad para trabajar en esta empresa");
+    }
+  }
+
+  //Escribiendo los getters y setters
+  public void getNombre(){
+    System.out.println("El nombre del empleado es: " + nombre);
+  }
+  public String setNombre(String nombre){
+    return this.nombre = nombre;
+  }
+
+  
+
+
 
 }
