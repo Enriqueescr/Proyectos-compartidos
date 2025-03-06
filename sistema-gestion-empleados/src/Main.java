ac4bd23eef66b7b75357a580cc6d7042de80259f
Enriqueescr <enrique.escribano@bootcamp-upgrade.com>
2025-03-06 13:24:10 +0100

    Update Main.java

--- sistema-gestion-empleados/src/Main.java
+++ sistema-gestion-empleados/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
   
        ArrayList <Empleado> listaEmpleados = new ArrayList<>();
         Desarrollador desarrollador2 = new Desarrollador("Dario", 20, 12000);
-        Gerente gerente2 = new Gerente("Avocado", 20, 1999);
-        DesarrolladorRemoto desarrolladorRemoto2 = new DesarrolladorRemoto("Daritoto", 20, 99999);
+        Gerente gerente2 = new Gerente("Enrique", 20, 1999);
+        DesarrolladorRemoto desarrolladorRemoto2 = new DesarrolladorRemoto("Jaun", 20, 99999);
 
          listaEmpleados.add(0, desarrollador2);
          listaEmpleados.add(1, gerente2 );
